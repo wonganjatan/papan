79c726a5552f255433cd694d80d5a9ee38bb0e79
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-16 18:59:59 +1000

    feat(frontend): add simple placeholder home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
-    <>
-      <div>
-        hellow
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+
+    return (
+        <main className="min-h-screen bg-neutral-50 dark:bg-neutral-950 text-neutral-900 dark:text-neutral-100">
+            <header className="border-b border-neutral-200 dark:border-neutral-800 px-8 py-5">
+                <h1 className="text-xl font-semibold tracking-tight">Papan</h1>
+                {/* Add another list */}
+            </header>
+            <section className="max-w-5xl mx-auto px-8 py-12">
+                
+            </section>
+        </main>
+    )
+}
